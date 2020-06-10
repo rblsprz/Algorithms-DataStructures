32de6bf12c3a39e94cea5ec4d41c10fcb202c14b
rblsprz <65626739+rblsprz@users.noreply.github.com>
2020-06-10 16:27:23 -0600

    Interfaces & Lógicas Primer Módulo

--- Algorithms&DataStructuresProject/src/GUI/Welcome.java
+++ Algorithms&DataStructuresProject/src/GUI/Welcome.java
@@ -49,7 +49,7 @@ private Timer time;
         lbEnter = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(2960, 1920));
+        setPreferredSize(new java.awt.Dimension(1500, 1500));
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowOpened(java.awt.event.WindowEvent evt) {
                 formWindowOpened(evt);
--- Algorithms&DataStructuresProject/src/Logic/Logic.java
+++ Algorithms&DataStructuresProject/src/Logic/Logic.java
@@ -5,6 +5,7 @@
  */
 package Logic;
 
+import Interfaces.LinkedList;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -13,6 +14,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 import javax.swing.JOptionPane;
 
@@ -237,6 +239,5 @@ public class Logic {
             JOptionPane.showMessageDialog(null, "¡PROBLEMAS DE ARCHIVO!");
         }//endCatch
         return array;
-    }//endCountry[]
-
+    }//End Roles[]
 }
