88092a03ca95ada72475f9db54ee45e7110b1c32
jesusvase <66399973+jesusvase@users.noreply.github.com>
2020-06-14 22:03:08 -0600

    Revert "Merge branch 'master' of https://github.com/rblsprz/Algorithms-DataStructures"
    This reverts commit d44d6df4f1ca444827b3aedcc322846b1477e11f, reversing
    changes made to 6ab82d9bbea25a7558e13c6f25ed91c116a929d0.

---
+++ Algorithms&DataStructuresProject/src/Interfaces/Stacks.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interfaces;
+
+/**
+ *
+ * @author pc
+ */
+public interface Stacks {
+    
+    public int size(); //Muestra el tamaño de la pila
+    public boolean isEmpty(); //Muestra si la pila esta vacia
+    public boolean isFull(); //Muestra si la pila tiene datos
+    public void push(int x); //Agregar
+    public int peek(); //Obtener el ultimo
+    public Object pop(); //Eliminar el ultimo
+    
+}
